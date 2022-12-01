a21e05a661b13a9a72bba2d57e228d80513bdd38
Kirubagar Thiagarajan <thiagarajan.k@northeastern.edu>
2022-12-01 16:33:54 -0500

    initialised classes

---
+++ Eatery/src/model/customer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ktkir
+ */
+
+public class customer {
+   private int customerId;
+private String email;
+private String mobile;
+private String address;
+private String city;
+//private ArrayList<Order> pastOrders;
+//private Order activeOrder;
+private String coupon;
+}
