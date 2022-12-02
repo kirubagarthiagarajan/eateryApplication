d25e1447fa15893f5c28923b36d77f2a3e5d50ec
barathis98 <114298089+barathis98@users.noreply.github.com>
2022-12-02 18:05:09 -0500

    Adding Customerlogin Screens

--- Eatery/src/model/customer.java
+++ Eatery/src/model/customer.java
@@ -12,12 +12,78 @@ import java.util.ArrayList;
  */
 
 public class customer {
-   private int customerId;
+private int customerId;
 private String email;
 private String mobile;
 private String address;
 private String city;
+private String Name;
+private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
 //private ArrayList<Order> pastOrders;
+    public void setCoupon(String coupon) {
+        this.coupon = coupon;
+    }
 //private Order activeOrder;
 private String coupon;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCoupon() {
+        return coupon;
+    }
 }
